6522c7e1713f7630bb7e82fe2cd85edf0628c7bc
Ryland Herrick <ryalnd@gmail.com>
2015-04-29 17:12:41 -0500

    Add service for generating an auth token
    Pretty simple wrapper around jsonwebtoken library.

---
+++ api/services/tokenAuth.js
@@ -0,0 +1,13 @@
+var jwt = require('jsonwebtoken');
+var popSecret = "I just wanted to share my pop secret with you!";
+var oneWeekInMinutes = 7 * 24 * 60;
+
+module.exports = {
+  sign: function(payload) {
+    return jwt.sign(payload, popSecret, {expiresInMinutes: oneWeekInMinutes});
+  },
+
+  verify: function(token, callback) {
+    return jwt.verify(payload, popSecret, {}, callback);
+  }
+};
